eade8b8ccf8973ead6587966a9c90628124987e6
Olalekan Ogundele <olalekan.o.ogundele@gmail.com>
2021-04-27 15:10:11 +0100

    Implemented feature to reset password when it's forgotten or lost

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    username = forms.CharField();
+    username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
