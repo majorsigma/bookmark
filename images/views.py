823748edf4bee0d5404a928e17c96eed6155a67d
Olalekan Ogundele <olalekan.o.ogundele@gmail.com>
2021-05-03 12:52:32 +0100

    Added feature to know the amount of views a bookmarked image has with the help of Redis

--- images/views.py
+++ images/views.py
@@ -10,7 +10,13 @@ from common.decorators import ajax_required
 from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from actions.utils import create_action
+import redis
+from django.conf import settings
 
+# connect to redis 
+r = redis.StrictRedis(host=settings.REDIS_HOST,
+                    port=settings.REDIS_PORT,
+                    db=settings.REDIS_DB)
 
 @login_required
 def image_create(request):
@@ -43,10 +49,15 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    # increment total image views by 1
+    total_views = r.incr('image:{}:views'.format(image.id))
+    # increment image ranking by 1
+    r.zincrby('image_ranking', image.id, 1)
     return render(request,
                 'images/image/detail.html',
                 {'section': 'images',
-                 'image': image})
+                 'image': image,
+                 'total_views': total_views})
 
 
 @ajax_required
@@ -99,4 +110,15 @@ def image_list(request):
                 'images/image/list.html',
                 {'section': 'images', 'images': images})
 
-            
+@login_required
+def image_ranking(request):
+    # get image ranking dictionary
+    image_ranking = r.zrange('image_ranking', 0, -1, desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    # get most viewed images 
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request,
+                'images/image/ranking.html',
+                {'section': 'images',
+                'most_viewed': most_viewed})
